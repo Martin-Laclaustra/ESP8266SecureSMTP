aa6772d1da4551ea22aed470e97600916036364d
Martin-Laclaustra <Martin-Laclaustra@github.com>
2019-03-24 01:21:01 +0100

    update example

--- examples/ESP8266SecureSMTP_Gmail/ESP8266SecureSMTP_Gmail.ino
+++ examples/ESP8266SecureSMTP_Gmail/ESP8266SecureSMTP_Gmail.ino
@@ -1,70 +1,49 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266SecureSMTP.h>
 
-const char* ssid = "";                  // WIFI network name
-const char* password = "";              // WIFI network password
-uint8_t connection_state = 0;           // Connected to WIFI or not
-uint16_t reconnect_interval = 10000;    // If not connected wait time to try again
+#ifndef STASSID
+#define STASSID "your-ssid"
+#define STAPSK  "your-password"
+#endif
 
-uint8_t WiFiConnect(const char* ssID, const char* nPassword)
-{
-  static uint16_t attempt = 0;
-  Serial.print("Connecting to ");
-  Serial.println(ssID);
-  WiFi.begin(ssID, nPassword);
-
-  uint8_t i = 0;
-  while(WiFi.status() != WL_CONNECTED && i++ < 50) {
-    delay(200);
-    Serial.print(".");
-  }
-  ++attempt;
-  Serial.println("");
-  if(i == 51) {
-    Serial.print(F("Connection: TIMEOUT on attempt: "));
-    Serial.println(attempt);
-    if(attempt % 2 == 0)
-      Serial.println(F("Check if access point available or SSID and Password are correct\r\n"));
-    return false;
-  }
-  Serial.println(F("Connection: ESTABLISHED"));
-  Serial.print(F("Got IP address: "));
-  Serial.println(WiFi.localIP());
-  return true;
-}
-
-void Awaits(uint16_t interval)
-{
-  uint32_t ts = millis();
-  while(!connection_state){
-    delay(50);
-    if(!connection_state && millis() > (ts + interval)){
-      connection_state = WiFiConnect(ssid, password);
-      ts = millis();
-    }
-  }
-}
+const char* ssid = STASSID;
+const char* password = STAPSK;
 
 void setup()
 {
   Serial.begin(115200);
-  delay(2000);
-  
-  connection_state = WiFiConnect(ssid, password);
 
-  if(!connection_state) {          // if not connected to WIFI
-    Awaits(reconnect_interval);    // constantly trying to connect
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("\nConnecting to ");
+  Serial.println(ssid);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(500);
   }
+  Serial.println();
+  Serial.print("connected, address=");
+  Serial.println(WiFi.localIP());
+  Serial.println();
 
   uint32_t startTime = millis();
   
   SMTP.setEmail("YOUR_EMAIL_ADDRESS@gmail.com")
     .setPassword("YOUR_PASSWORD")
-    .Subject("ESP8266SMTP Gmail test")
-    .setFrom("ESP8266SMTP")
-    .setForGmail();   // simply sets port to 465 and setServer("smtp.gmail.com");            
+    .Subject("ESP8266SecureSMTP Gmail test")
+    .setFrom("ESP8266SecureSMTP")
+    .setForGmail();    // simply sets port to 465 and setServer("smtp.gmail.com");
+
+  //SMTP.setInsecure(); // Alternatively, uncomment this and comment SMTP.setFingerprint below
+
+  const uint8_t fingerprint[] = {0x76, 0xc4, 0xac, 0x8e, 0xe5, // smtp.gmail.com fingerprint
+                                 0x92, 0x2b, 0x5e, 0x73, 0x82,
+                                 0x12, 0xf5, 0x70, 0x26, 0xf5,
+                                 0x28, 0x1c, 0xb3, 0x64, 0xe1};
+  SMTP.setFingerprint(fingerprint);
+
                       // message text from http://www.blindtextgenerator.com/lorem-ipsum
-  if(SMTP.Send("Recipient Email, use comma for multiple recipients", "The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in their grammar, their pronunciation and their most common words. Everyone realizes why a new common language would be desirable: one could refuse to pay expensive translators. To achieve this, it would be necessary to have uniform grammar, pronunciation and more common words. If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages. The new common language will be more simple and regular than the existing European languages. It will be as simple as Occidental; in fact, it will be Occidental. To an English person, it will seem like simplified English, as a skeptical Cambridge friend of mine told me what Occidental is. The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in their grammar, their pronunciation and their most common words. Everyone realizes why a new common language would be desirable: one could refuse to pay expensive translators. To achieve this, it would be necessary to have uniform grammar, pronunciation and more common words. If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages. The new common language will be more simple and regular than the existing European languages. It will be as simple as Occidental; in fact, it will be Occidental. To an English person, it will seem like simplified English, as a skeptical Cambridge friend of mine told me what Occidental is. The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in their grammar, their pronunciation and their most common words. Everyone realizes why a new common language would be desirable: one could refuse to pay expensive translators. To achieve this, it would be necessary to have uniform grammar, pronunciation and more common words. If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages. The new common language will be more simple and regular than the existing European languages. It will be as simple as Occidental; in fact, it will be Occidental. To an English person, it will seem like simplified English, as a skeptical Cambridge friend of mine told me what Occidental is.The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in their grammar, their pronunciation and their most common words. Everyone realizes why a new common language would be desirable: one could refuse to pay expensive translators. To achieve this, it would be necessary to have uniform grammar, pronunciation and more common ")) {
+  if(SMTP.Send("Recipient Email, use comma for multiple recipients", "The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in their grammar, their pronunciation and their most common words. Everyone realizes why a new common language would be desirable: one could refuse to pay expensive translators. To achieve this, it would be necessary to have uniform grammar, pronunciation and more common words. If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages. The new common language will be more simple and regular than the existing European languages. It will be as simple as Occidental; in fact, it will be Occidental. To an English person, it will seem like simplified English, as a skeptical Cambridge friend of mine told me what Occidental is. Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar. The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way. When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then she continued her way. On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word and and the Little Blind Text should turn around and return to its own, safe country. But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole and dragged her into their agency, where they abused her for their projects again and again. And if she hasn’t been rewritten, then they are still using her. A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents.")) {
     Serial.println(F("Message sent"));
   } else {
     Serial.print(F("Error sending message: "));
